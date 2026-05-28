0cb8780580058763aef672d99d9c79c1551a1e26
hanhphucviet <huyron03@gmail.com>
2026-05-28 22:36:34 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/QL-Kho-Mr-Kien/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
